77e06982525817882f197885e0dff21190c68afc
Masa0u0 <masa0u0masa1215@gmail.com>
2022-11-21 11:52:21 +0900

    ディレクトリ構成を変更

--- setup.py
+++ setup.py
@@ -13,19 +13,19 @@ install_requires = [
 ext_modules = [
     Pybind11Extension(
         "_encoder",   # PYBIND11_MODULEの名前と一致している必要がある
-        sorted(glob("src/encoder/*.cpp")),
+        sorted(glob("src/cpp/encoder/*.cpp")),
     ),
     Pybind11Extension(
         "_liquid_state_machine",
-        sorted(glob("src/liquid_state_machine/*.cpp")),
+        sorted(glob("src/cpp/liquid_state_machine/*.cpp")),
     ),
     Pybind11Extension(
         "_ext_lsm",
-        sorted(glob("src/ext_lsm/*.cpp")),
+        sorted(glob("src/cpp/ext_lsm/*.cpp")),
     ),
     Pybind11Extension(
         "_reservoir",
-        sorted(glob("src/reservoir/*.cpp")),
+        sorted(glob("src/cpp/reservoir/*.cpp")),
     ),
 ]
 
@@ -33,8 +33,8 @@ setup(
     name="dh_reservoir",
     version="0.0.0",
     author="dohi",
-    packages=find_packages(where="lib"),   # lib内の全てをパッケージとみなす(libをルートとする)
-    package_dir={"": "lib"},   # ここに共有ファイルが作成される
+    packages=find_packages(where="src/python"),   # このディレクトリ以下をパッケージとする
+    package_dir={"": "src/python"},   # ここに共有ファイルが作成される
     install_requires=install_requires,
     ext_modules=ext_modules,
 )
